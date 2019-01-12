d88d1ff30237c06480975a69af57ed19e0f3b14e
Miłosz Panas <miloszpanas@gmail.com>
2019-01-12 18:55:22 +0100

    Add wrapper to comment

--- src/index.js
+++ src/index.js
@@ -3,27 +3,34 @@ import ReactDOM from "react-dom";
 import faker from "faker";
 
 import CommentDetail from "./CommentDetail"; 
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        at="Todat at 4pm" 
-        image={faker.image.avatar()} 
-        text="this is my first post!"
-      />
-      <CommentDetail 
-        author="Brian" 
-        at="Todat at 5pm" 
-        image={faker.image.avatar()} 
-        text="yeah. so is mine"
-      />
-      <CommentDetail 
-        author="Michael"at="Todat at 6pm" 
-        image={faker.image.avatar()} 
-        text="sure, sure, liers"
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          at="Todat at 4pm" 
+          image={faker.image.avatar()} 
+          text="this is my first post!"
+        />
+      </ApprovalCard> 
+      <ApprovalCard>
+        <CommentDetail 
+          author="Brian" 
+          at="Todat at 5pm" 
+          image={faker.image.avatar()} 
+          text="yeah. so is mine"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Michael"at="Todat at 6pm" 
+          image={faker.image.avatar()} 
+          text="sure, sure, liers"
+        />
+      </ApprovalCard>
     </div>
   );
 };
